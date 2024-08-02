9481f063f0a3c7e4c2864de6d6e75fe6de10319d
phvdat <dat.pham@lfglobaltech.com>
2024-08-02 14:54:46 +0700

    feat: update header

--- src/app/(page)/MainLayout.tsx
+++ src/app/(page)/MainLayout.tsx
@@ -1,75 +1,30 @@
 'use client';
-import { LogoutOutlined } from '@ant-design/icons';
-import { Button, Divider, Flex, Menu, Typography } from 'antd';
+import Header from '@/components/header/Header';
+import { Layout, Typography } from 'antd';
 import moment from 'moment';
-import { signOut } from 'next-auth/react';
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
+import { SessionProvider } from 'next-auth/react';
 import { PropsWithChildren } from 'react';
-const { Title, Text } = Typography;
+const { Text } = Typography;
+const { Content, Footer } = Layout;
 
 export default function MainLayout({ children }: PropsWithChildren) {
-  const items = [
-    {
-      label: (
-        <Link href='/home'>
-          <Title level={5}>Home</Title>
-        </Link>
-      ),
-      key: '/home',
-    },
-    {
-      label: (
-        <Link href='/woo'>
-          <Title level={5}>Woo tool</Title>
-        </Link>
-      ),
-      key: '/woo',
-      children: [
-        {
-          label: <Link href='/woo/config-categories'>Config Categories</Link>,
-          key: '/woo/config-categories',
-        },
-        {
-          label: <Link href='/woo/config-watermark'>Config Watermark</Link>,
-          key: '/woo/config-watermark',
-        },
-      ],
-    },
-  ];
-
-  const pathName = usePathname();
   return (
-    <Flex
-      style={{ padding: '0 20px', minHeight: '100vh' }}
-      vertical
-      justify='space-between'
-    >
-      <div>
-        <Flex
-          justify='space-between'
-          style={{ marginBottom: 24 }}
-          align='center'
-        >
-          <Menu
-            style={{ minWidth: 0, flex: 'auto' }}
-            mode='horizontal'
-            selectedKeys={[pathName]}
-            items={items}
-            triggerSubMenuAction='hover'
-          />
-          <Button onClick={() => signOut()}>
-            <LogoutOutlined />
-          </Button>
-        </Flex>
-        {children}
-      </div>
-      <div>
-        <Divider />
-        <Text type='secondary'>
-          Copyright by deveric {moment().format('YYYY')}
-        </Text>
-      </div>
-    </Flex>
+    <SessionProvider>
+      <Layout style={layoutStyle}>
+        <Header />
+
+        <Content>{children}</Content>
+
+        <Footer>
+          <Text type='secondary'>
+            Copyright by deveric {moment().format('YYYY')}
+          </Text>
+        </Footer>
+      </Layout>
+    </SessionProvider>
   );
 }
+
+const layoutStyle: React.CSSProperties = {
+  minHeight: '100vh',
+};
