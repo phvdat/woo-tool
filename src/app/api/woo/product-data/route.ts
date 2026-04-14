b37fbaba9a08456632bd5e6aded41862e8a8e857
phvdat <phamvandat.hcmut@gmail.com>
2026-04-14 11:14:37 +0700

    update(origin product): impove search with mongo alas index

--- src/app/api/woo/product-data/route.ts
+++ src/app/api/woo/product-data/route.ts
@@ -10,27 +10,34 @@ export async function GET(request: Request) {
   const email = searchParams.get('email') || '';
   const categories = searchParams.get('categories') || '';
   const name = searchParams.get('name') || '';
+
   let { db } = await connectToDatabase();
-  const filter: any = {
-    email,
-  };
-  if (categories) {
-    filter.Categories = {
-      $regex: categories,
-      $options: 'i',
-    };
-  }
-  if (name) {
-    filter.Name = {
-      $regex: name,
-      $options: 'i',
-    };
-  }
-  const response = await db
-    .collection(PRODUCT_DATA_COLLECTION)
-    .find(filter)
-    .sort({ createdAt: -1 })
-    .toArray();
+
+  const response = await db.collection(PRODUCT_DATA_COLLECTION).aggregate([
+    {
+      $search: {
+        index: "product",
+        text: {
+          query: name,
+          path: "Name",
+          fuzzy: { maxEdits: 2 }
+        }
+      }
+    },
+    {
+      $match: {
+        email,
+        ...(categories ? { Categories: categories } : {})
+      }
+    },
+    {
+      $sort: {
+        score: { $meta: "searchScore" }
+      }
+    }
+  ]).toArray();
+  console.log(response);
+  
   return Response.json(response, { status: 200 });
 }
 
